7b9b49fb6122bd44f2968e27de1c398e47a69a17
nacho <ignacioanefra@gmail.com>
2021-04-14 23:09:39 +0200

    Installing mongoose and connection with DB

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
-//adding config variables to the process
-const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv'); //for adding config variables to the process
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection established'));
 
 //after run the app file
 const app = require('./index');
