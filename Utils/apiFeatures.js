c6eef40cefb8c906b812b4e6fd2846cee70a6eea
nacho <ignacioanefra@gmail.com>
2021-04-20 21:59:12 +0200

    Export ApiFeatures to another module

---
+++ Utils/apiFeatures.js
@@ -0,0 +1,46 @@
+module.exports = class APIFeatures {
+  constructor(entityModel, reqQuery) {
+    this.entityModel = entityModel;
+    this.reqQuery = reqQuery;
+  }
+
+  filter() {
+    const queryObj = { ...this.reqQuery }; // harcoded copy, not just reference, for modify
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+    // how gte or similar filters: in mongoDB { rating: { $get: 8} }
+    // express give us a query almost equal, without $ mongo operator, if we use in the url: ?rating[gte]=8
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    this.entityQuery = this.entityModel.find().where(JSON.parse(queryStr));
+    // to return the entire object and then chain with others features
+    return this;
+  }
+
+  sort() {
+    if (this.reqQuery.sort) {
+      // separated the values with commas, could be negative values for desc criteria
+      const sortBy = this.reqQuery.sort.split(',').join(' ');
+      this.entityQuery = this.entityQuery.sort(sortBy);
+    }
+    return this;
+  }
+
+  limitFields() {
+    if (this.reqQuery.fields) {
+      // negative value means exclusion instead desired fields
+      const fields = this.reqQuery.fields.split(',').join(' ');
+      this.entityQuery = this.entityQuery.select(fields);
+    }
+    return this;
+  }
+
+  paginate() {
+    // pagination, default value page=1 & limit=100
+    const page = this.reqQuery.page * 1 || 1;
+    const limit = this.reqQuery.limit * 1 || 100;
+    const resultsSkipped = (page - 1) * limit;
+    this.entityQuery = this.entityQuery.skip(resultsSkipped).limit(limit);
+    return this;
+  }
+};
