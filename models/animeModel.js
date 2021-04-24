afbe8ed5f0f542dea5187b3827934f825fa20be8
nacho <ignacioanefra@gmail.com>
2021-04-24 18:44:32 +0200

    Data model Validation

--- models/animeModel.js
+++ models/animeModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const genres = require('../utils/genres');
 
 const animeSchema = new mongoose.Schema(
   {
@@ -6,6 +7,7 @@ const animeSchema = new mongoose.Schema(
       type: String,
       required: [true, 'An anime must have a name'],
       unique: true,
+      trim: true,
     },
     synopsis: {
       type: String,
@@ -14,14 +16,23 @@ const animeSchema = new mongoose.Schema(
     imageUrl: {
       type: String,
       required: [true, 'An image its mandatory'],
+      validate: {
+        validator: function (val) {
+          const splittedValue = val.split('.');
+          return splittedValue[splittedValue.length - 1] === 'jpg';
+        },
+        message: '({VALUE}) not accepted. Only ".jpg" format is accepted',
+      },
     },
     rating: {
       type: Number,
       default: 5,
+      min: [1, 'Rating must be above 0'],
+      max: [10, 'Rating must be below 10'],
     },
     malId: {
       type: Number,
-      required: [true, 'The data is imported from mal, where must have an id'],
+      required: [true, 'The data is imported from mal, where it must have an id'],
     },
     episodes: Number,
     year: Number,
@@ -31,6 +42,7 @@ const animeSchema = new mongoose.Schema(
     genres: [
       {
         type: String,
+        enum: genres,
       },
     ],
   },
@@ -58,12 +70,13 @@ animeSchema.virtual('numberGenres').get(function () {
 // });
 
 // Query middelwares: 'this' points to query object
-animeSchema.pre(/^find/, function (next) {
+animeSchema.pre(/(?=^find)(?=(?!findOneAndUpdate))/, function (next) {
+  console.log('ENTRA');
   this.find({ title: { $ne: 'PRUEBA' } });
   this.start = Date.now();
   next();
 });
-animeSchema.post(/^find/, function (docs, next) {
+animeSchema.post(/(?=^find)(?=(?!findOneAndUpdate))/, function (docs, next) {
   console.log(`Query took ${Date.now() - this.start} milliseconds`);
   next();
 });
